70b8e5986904d88f57a5a517a64f4e0a6401d40c
KimzaTech <kimzatech@gmail.com>
2022-08-18 22:41:06 +0300

    app.js

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ const DUMMY_DATA = [
 const CHART_WIDTH = 600;
 const CHART_HEIGHT = 400;
 
+const x = d3.scaleBand()
+.rangeRound([0, CHART_WIDTH])
+.padding(0.1);
+
+const y = d3.scaleLinear()
+.range([CHART_HEIGHT, 0]);
+
 const chartContainer = d3.select('svg')
 .attr('width', CHART_WIDTH)
 .attr('height', CHART_HEIGHT); //container that holds my chart
+
+x.domain(DUMMY_DATA.map((d) => d.region));
 
 const chart = chartContainer.append('g');
 
