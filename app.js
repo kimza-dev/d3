c0152ef3ffbe5bea8e6daac8f21264e36b78b59c
KimzaTech <kimzatech@gmail.com>
2022-08-18 22:29:06 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const DUMMY_DATA = [
+    { id:'d1', region: 'Kenya', value:10},
+    { id:'d2', region: 'Uganda', value:12},
+    { id:'d3', region: 'Ethiopia', value:11},
+    { id:'d4', region: 'Tanzania', value:6},
+];
