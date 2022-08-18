9d402ba15e6d501353e17a1422f5fa45b6e891c0
KimzaTech <kimzatech@gmail.com>
2022-08-18 22:33:20 +0300

    app.js

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const DUMMY_DATA = [
     { id:'d3', region: 'Ethiopia', value:11},
     { id:'d4', region: 'Tanzania', value:6},
 ];
+
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400;
+
+const chartContainer = d3.select('svg')
+.attr('width', CHART_WIDTH)
+.attr('height', CHART_HEIGHT); //container that holds my chart
